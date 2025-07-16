e3f928bbc0d634363309428cda8de16378fd9f52
ky-hiverge <kante@hiverge.ai>
2025-07-16 16:55:43 +0100

    Add CLIs
    Signed-off-by: ky-hiverge <kante@hiverge.ai>

--- src/hive_cli/main.py
+++ src/hive_cli/main.py
@@ -1,80 +1,45 @@
 import argparse
 
-from datetime import datetime
-import yaml
+from .platform.k8s import K8sPlatform
+from .platform.onprem import OnPremPlatform
 
-def init(args):
-    print("(Unimplemented) Initializing hive...")
-
-def create(args):
-    config_file = args.config
-
-    try:
-        with open(config_file, 'r') as file:
-            config = yaml.safe_load(file)
-            print(f"Configuration loaded successfully")
-    except FileNotFoundError:
-        print(f"Error: Config file '{config_file}' not found.")
-        return
-    except yaml.YAMLError as e:
-        print(f"Error parsing YAML configuration: {e}")
-        return
-
-    if not validate_create(config):
-        return
-
-
-def validate_create(config):
-    if not config.get("name") and not config.get("generateName"):
-        print("Error: Either 'name' or 'generateName' must be specified in the configuration.")
-        return False
-
-def get_name(config) -> str:
-    if config.get("name"):
-        return config.get("name")
-    elif config.get("generateName"):
-        return config.get("generateName").format(datetime.now().strftime('%Y-%m-%d_%H%M%S'))
-    # We should not reach here.
-    return ""
-
-def delete(args):
-    print("Deleting hive...")
-
-def login(args):
-    print("(Unimplemented) Logging in to hive...")
-
-def show_experiments(args):
-    print("(Unimplemented) Showing experiments...")
+PLATFORMS = {
+    "k8s": K8sPlatform,
+    "on-prem": OnPremPlatform,
+}
 
 def main():
     parser = argparse.ArgumentParser(description="Hive CLI")
+    parser.add_argument("-p", "--platform", default="k8s", choices=PLATFORMS.keys(), help="Platform to use, k8s or on-prem, default to use k8s")
     subparsers = parser.add_subparsers(dest="command", required=True)
 
     # init command
-    parser_init = subparsers.add_parser("init", help="Initialize hive")
-    parser_init.set_defaults(func=init)
+    parser_init = subparsers.add_parser("init", help="Initialize a repository")
+    parser_init.set_defaults(func=lambda args: PLATFORMS[args.platform]().init(args))
 
     # create command
-    parser_create = subparsers.add_parser("create", help="Create a new hive")
-    parser_create.add_argument("--config", required=False, help="Path to the hive configuration file, default to `hive.yaml`", default="hive.yaml")
-    parser_create.add_argument("--platform", required=False, help="Platform to deploy the hive", default="k8s", choices=["k8s", "on-prem"])
-    parser_create.set_defaults(func=create)
+    parser_create = subparsers.add_parser("create", help="Create a new experiment")
+    parser_create.add_argument("name", help="Name of the experiment to create. If ends with `-`, it will be generated with a timestamp suffix like `exper-2023-10-01_123456`")
+    parser_create.add_argument("-f", "--config", required=True, help="Path to the experiment configuration file")
+    parser_create.set_defaults(func=lambda args: PLATFORMS[args.platform]().create(args))
 
     # delete command
-    parser_delete = subparsers.add_parser("delete", help="Delete a hive")
-    parser_delete.set_defaults(func=delete)
+    parser_delete = subparsers.add_parser("delete", help="Delete a experiment")
+    parser_delete.add_argument("name", help="Name of the experiment to delete")
+    parser_delete.set_defaults(func=lambda args: PLATFORMS[args.platform]().delete(args))
 
-    # login command
-    parser_login = subparsers.add_parser("login", help="Login to hive")
-    parser_login.set_defaults(func=login)
+    # TODO:
+    # # login command
+    # parser_login = subparsers.add_parser("login", help="Login to Hive")
+    # parser_login.set_defaults(func=lambda args: PLATFORMS[args.platform]().login(args))
 
-    # show command
-    parser_show = subparsers.add_parser("show", help="Show resources")
-    show_subparsers = parser_show.add_subparsers(dest="subcommand", required=True)
+    # # show command
+    # parser_show = subparsers.add_parser("show", help="Show resources")
+    # show_subparsers = parser_show.add_subparsers(dest="subcommand", required=True)
 
-    # show experiments command
-    parser_show_experiments = show_subparsers.add_parser("experiments", help="Show experiments")
-    parser_show_experiments.set_defaults(func=show_experiments)
+    # # show experiments command
+    # parser_show_experiments = show_subparsers.add_parser("experiments", help="Show experiments")
+    # parser_show_experiments.set_defaults(func=lambda args: PLATFORMS[args.platform]().show_experiments(args))
 
     args = parser.parse_args()
     args.func(args)
